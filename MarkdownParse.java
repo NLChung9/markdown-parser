ae159cd2f4351294d6a28718c7e507c039a54e67
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-04-21 08:58:48 -0700

    Fixed test-file-copy2 bug
    Checked for a period inside the parentheses, continues loop if there is no period or a period AFTER the closing parenthesis.
    
    Each time the list continues, currentIndex increments, so the code will eventually hit every link, or every section with a period in it.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,27 +11,43 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        boolean contSwitch = false;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            //If there is no more openBracket in the text, break out of the loop.
+            //If there is no more openBracket in the text, set the contSwitch on.
             if (openBracket == -1){
-                break;
+                contSwitch = true;
             }
             int closeBracket = markdown.indexOf("]", openBracket);
-            //If there is no more closeBracket in the text, break out of the loop.
+            //If there is no more closeBracket in the text, set the contSwitch on.
             if (closeBracket == -1){
-                break;
+                contSwitch = true;
             }
             int openParen = markdown.indexOf("(", closeBracket);
-            //If there is no more openParen in the text, break out of the loop.
+            //If there is no more openParen in the text, set the contSwitch on.
             if (openParen == -1){
-                break;
+                contSwitch = true;
             }
             int closeParen = markdown.indexOf(")", openParen);
-            //If there is no more closeParen in the text, break out of the loop.
+            //If there is no more closeParen in the text, set the contSwitch on.
             if (closeParen == -1){
-                break;
+                contSwitch = true;
             }
+            int period = markdown.indexOf(".", openParen);
+            //If there is no more period in the text OR a period after
+            //the closeParen, set the contSwitch on.
+            if (period == -1 || period > closeParen){
+                contSwitch = true;
+            }
+
+            //IF contSwitch is TRUE, increment currentIndex and
+            //skip to the next iteration of the loop.
+            if (contSwitch){
+                currentIndex++;
+                contSwitch = false;
+                continue;
+            }
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             System.out.println(currentIndex);
--- test-file-copy2.md
+++ test-file-copy2.md
@@ -1,5 +1,6 @@
 # Title
 
 [link1](https://something.com)
-[not a link](not a link)
+[not a link](not a link) [link3](slither.io)
+[link2](some-thing.html)
 
