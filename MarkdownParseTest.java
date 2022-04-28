0c47fb1ecf97d8f50b218041f6f09043d5d8eaae
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-04-28 08:45:45 -0700

    Added several more test files, renamed test files, added one failure-inducing input

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,45 +18,74 @@ public class MarkdownParseTest extends MarkdownParse{
 
     @Test
     public void test1() throws IOException{
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("test-file1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
         ArrayList<String> expected = new ArrayList<String>();
         expected.add("https://something.com");
         expected.add("some-thing.html");
-        assertEquals(links, expected);
+        assertEquals(expected, links);
     }
 
     @Test
     public void test2() throws IOException{
-        Path fileName = Path.of("test-file-copy.md");
+        Path fileName = Path.of("test-file2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
         ArrayList<String> expected = new ArrayList<String>();
         expected.add("https://something.com");
         expected.add("some-thing.html");
-        assertEquals(links, expected);
+        assertEquals(expected, links);
     }
 
     @Test
     public void test3() throws IOException{
-        Path fileName = Path.of("test-file-copy2.md");
+        Path fileName = Path.of("test-file3.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
         ArrayList<String> expected = new ArrayList<String>();
         expected.add("https://something.com");
         expected.add("https://slither.io");
         expected.add("https://some-thing.html");
-        assertEquals(links, expected);
+        assertEquals(expected, links);
     }
 
     @Test
     public void test4() throws IOException{
-        Path fileName = Path.of("test-file-copy3.md");
+        Path fileName = Path.of("test-file4.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
         ArrayList<String> expected = new ArrayList<String>();
         expected.add("https://something.com");
-        assertEquals(links, expected);
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void test5() throws IOException{
+        Path fileName = Path.of("test-file5.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void test6() throws IOException{
+        Path fileName = Path.of("test-file6.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void test7() throws IOException{
+        Path fileName = Path.of("test-file7.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, links);
     }
 }
